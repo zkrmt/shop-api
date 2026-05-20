dfa85e62fb14ca4cdaf4bfe4deb26066ceb87d11
zkrmt <airakazmin@gmail.com>
2026-05-20 21:22:24 +0200

    resolve pm2 probleme in stripe.js

--- config/stripe.js
+++ config/stripe.js
@@ -1,6 +1,11 @@
 import Stripe from "stripe"
 import dotenv from "dotenv"
-dotenv.config()
+import { fileURLToPath } from "url"
+import { dirname, join } from "path"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+dotenv.config({ path: join(__dirname, "../.env") })
 
 const stripe =  new Stripe(process.env.STRIPE_SECRET_KEY)
 
