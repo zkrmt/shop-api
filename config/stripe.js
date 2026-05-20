cc812ee3d1030ef86b37e1d26d108874b99252bb
zkrmt <airakazmin@gmail.com>
2026-05-20 21:26:43 +0200

    update dotenv in stripe.js

--- config/stripe.js
+++ config/stripe.js
@@ -1,11 +1,7 @@
 import Stripe from "stripe"
 import dotenv from "dotenv"
-import { fileURLToPath } from "url"
-import { dirname, join } from "path"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-dotenv.config({ path: join(__dirname, "../.env") })
+dotenv.config({ path: "/var/www/shop-api/.env" })
 
 const stripe =  new Stripe(process.env.STRIPE_SECRET_KEY)
 
